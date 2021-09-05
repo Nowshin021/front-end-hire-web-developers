eb20a71c99eedc0e962c4b6227b8054fcde72b4a
Nowwshin <nowshinwork3247@gmail.com>
2021-09-06 03:13:36 +0600

    added some front end design and footer

---
+++ main.js
@@ -0,0 +1,10 @@
+function openForm_signup(){
+    
+    //document.getElementById("mySignupForm").style.display = "block";
+    //document.getElementById("landing-section").style.display="none"
+}
+
+function closeForm_signup() {
+   //document.getElementById("mySignupForm").style.display = "none";
+    //document.getElementById("landing-section").style.display="grid"
+}
