722a11d19e2d5a097aa05a2555db8c8f8fa72c1b
Nowwshin <nowshinwork3247@gmail.com>
2021-09-07 00:23:10 +0600

    added sign up log in pop up form

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 function openForm_signup(){
     
     //document.getElementById("mySignupForm").style.display = "grid";
-   // document.getElementById("landing-section").style.display="none"
+   document.getElementById("landing-section").style.display="none";
+   document.getElementById("hidden-form-section-login").style.display="none";
+   document.getElementById("hidden-form-section").style.display="grid";
+
     //document.getElementById("intro-section").style.display="none"
 }
 
-function closeForm_signup() {
-    //document.getElementById("mySignupForm").style.display = "none";
-    //document.getElementById("landing-section").style.display="grid";
+function Cancel() {
+    document.getElementById("landing-section").style.display = "grid";
+    document.getElementById("hidden-form-section").style.display="none";
+    document.getElementById("hidden-form-section-login").style.display="none";
     //document.getElementById("intro-section").style.display="grid"
+}
+
+function openForm_login(){
+    document.getElementById("landing-section").style.display="none";
+    document.getElementById("hidden-form-section").style.display="none";
+    document.getElementById("hidden-form-section-login").style.display="grid";
+    
+
+}
+
+
+function send_message(){
+    alert("Thank you for your feedback")
 }
