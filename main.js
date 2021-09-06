e36ad0552b039879fc92f2fae3b36f8349e7f5fa
Nowwshin <nowshinwork3247@gmail.com>
2021-09-06 17:45:16 +0600

    Added Contact us form

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 function openForm_signup(){
     
-    //document.getElementById("mySignupForm").style.display = "block";
-    //document.getElementById("landing-section").style.display="none"
+    //document.getElementById("mySignupForm").style.display = "grid";
+   // document.getElementById("landing-section").style.display="none"
+    //document.getElementById("intro-section").style.display="none"
 }
 
 function closeForm_signup() {
-   //document.getElementById("mySignupForm").style.display = "none";
-    //document.getElementById("landing-section").style.display="grid"
+    //document.getElementById("mySignupForm").style.display = "none";
+    //document.getElementById("landing-section").style.display="grid";
+    //document.getElementById("intro-section").style.display="grid"
 }
